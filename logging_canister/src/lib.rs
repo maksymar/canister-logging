7da37588e1322e247cc8c6077f8914d2cab22d3d
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-03-14 12:24:51 +0000

    cleanup

--- logging_canister/src/lib.rs
+++ logging_canister/src/lib.rs
@@ -16,12 +16,12 @@ fn explicit_panic() {
 }
 
 #[ic_cdk::update]
-fn failed_unwrap() {
-    String::from_utf8(vec![0xc0, 0xff, 0xee]).unwrap();
-}
-
-#[ic_cdk::update]
 fn memory_oob() {
     let mut buffer = vec![0u8; 10];
     ic_cdk::api::stable::stable_read(20, &mut buffer);
+}
+
+#[ic_cdk::update]
+fn failed_unwrap() {
+    String::from_utf8(vec![0xc0, 0xff, 0xee]).unwrap();
 }
