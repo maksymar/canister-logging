5556a67c3fbb7d3579c48a517de4142af90a41b2
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-03-14 11:34:08 +0000

    init

---
+++ logging_canister/src/lib.rs
@@ -0,0 +1,33 @@
+#[ic_cdk::update]
+fn greet(name: String) -> String {
+    let message = format!("Hello, {}!", name);
+    ic_cdk::print(message.clone());
+    message
+}
+
+#[ic_cdk::update]
+fn explicit_trap(message: String) {
+    ic_cdk::trap(&message);
+}
+
+#[ic_cdk::update]
+fn explicit_panic() {
+    panic!("panic attack");
+}
+
+#[ic_cdk::update]
+fn failed_unwrap() {
+    String::from_utf8(vec![0xc0, 0xff, 0xee]).unwrap();
+}
+
+#[ic_cdk::update]
+fn memory_oob() {
+    let mut buffer = vec![0u8; 10];
+    ic_cdk::api::stable::stable_read(20, &mut buffer);
+}
+
+// #[ic_cdk::heartbeat]
+// async fn heartbeat() {
+//     //ic_cdk::trap("heartbeat trap");
+//     String::from_utf8(vec![0xc0, 0xff, 0xee]).unwrap();
+// }
