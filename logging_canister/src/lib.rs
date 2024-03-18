8fdd5fe0113ae42018f5913c2127c88c85c850a1
Maksym Arutyunyan <maksym.arutyunyan@dfinity.org>
2024-03-18 14:36:52 +0000

    wording

--- logging_canister/src/lib.rs
+++ logging_canister/src/lib.rs
@@ -1,3 +1,12 @@
+// use std::panic;
+
+// #[ic_cdk::init]
+// fn init() {
+//     panic::set_hook(Box::new(|_| {
+//         // Do nothing to avoid duplicated panic messages.
+//     }));
+// }
+
 #[ic_cdk::update]
 fn greet(name: String) -> String {
     let message = format!("Hello, {}!", name);
@@ -7,31 +16,31 @@ fn greet(name: String) -> String {
 
 #[ic_cdk::update]
 fn explicit_trap(message: String) {
-    ic_cdk::print("message before trap is preserved");
+    ic_cdk::print("Message before trap is preserved");
     ic_cdk::trap(&message);
 }
 
 #[ic_cdk::update]
 fn explicit_panic() {
-    ic_cdk::print("message before panic is preserved");
+    ic_cdk::print("Message before panic is preserved");
     panic!("panic attack");
 }
 
 #[ic_cdk::update]
 fn memory_oob() {
-    ic_cdk::print("message before memory oob is preserved");
+    ic_cdk::print("Message before memory oob is preserved");
     let mut buffer = vec![0u8; 10];
     ic_cdk::api::stable::stable_read(20, &mut buffer);
 }
 
 #[ic_cdk::update]
 fn failed_unwrap() {
-    ic_cdk::print("message before failed unwrap is preserved");
+    ic_cdk::print("Message before failed unwrap is preserved");
     String::from_utf8(vec![0xc0, 0xff, 0xee]).unwrap();
 }
 
 // #[ic_cdk::heartbeat]
 // fn heartbeat() {
-//     ic_cdk::print("message before heartbeat trap is preserved");
-//     ic_cdk::trap("heartbeat trap");
+//     ic_cdk::print("Message before heartbeat trap is preserved");
+//     ic_cdk::trap("Heartbeat trap");
 // }
